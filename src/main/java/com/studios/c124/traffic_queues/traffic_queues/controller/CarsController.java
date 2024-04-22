7d85b774d22ebd21b01d4cd996d9957681b9be02
Steven-Leon-007 <estivlinyor@gmail.com>
2024-04-22 15:49:38 -0500

    started the design

--- src/main/java/com/studios/c124/traffic_queues/traffic_queues/controller/CarsController.java
+++ src/main/java/com/studios/c124/traffic_queues/traffic_queues/controller/CarsController.java
@@ -1,10 +1,23 @@
 package com.studios.c124.traffic_queues.traffic_queues.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.studios.c124.traffic_queues.traffic_queues.service.CarsService;
 
 @Controller
 @RequestMapping()
 public class CarsController {
-    
+
+    // @Autowired
+    // private CarsService carsService;
+
+    @GetMapping("/")
+    public ModelAndView index(){
+        ModelAndView model = new ModelAndView("index");
+        return model;
+    }
 }
