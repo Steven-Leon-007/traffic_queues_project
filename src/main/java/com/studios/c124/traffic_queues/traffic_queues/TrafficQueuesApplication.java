649d6939df508685e093daf6dedfa483f1de4456
Steven-Leon-007 <estivlinyor@gmail.com>
2024-04-22 15:13:38 -0500

    Firsr commit

---
+++ src/main/java/com/studios/c124/traffic_queues/traffic_queues/TrafficQueuesApplication.java
@@ -0,0 +1,13 @@
+package com.studios.c124.traffic_queues.traffic_queues;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrafficQueuesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrafficQueuesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/studios/c124/traffic_queues/traffic_queues/TrafficQueuesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studios.c124.traffic_queues.traffic_queues;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrafficQueuesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
